b91711e0145a733c7d72414b030fb94334483d22
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-01-12 10:30:48 +0800

    Added mock for todos

---
+++ utils/fixtures/todos.js
@@ -0,0 +1,21 @@
+
+const Todos = [
+{
+  "todo_id": 0,
+  "todo": "Get some milk",
+  "author": "Jim",
+  "date_created": "2016-11-01T23:15:00.000Z",
+  "duedate": "2016-11-08T08:00:00.000Z",
+  "completed": false
+},
+{
+  "todo_id": 1,
+  "todo": "Get some cereal",
+  "author": "Austin",
+  "date_created": "2016-11-01T23:15:00.000Z",
+  "duedate": "2016-11-08T08:00:00.000Z",
+  "completed": false
+}
+]
+
+module.exports = Todos
